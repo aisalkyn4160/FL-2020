4a8d0047e7885c4ad1a0bbaa59486036ded61a4b
aisalkyn4160 <67801790+aisalkyn4160@users.noreply.github.com>
2020-08-14 03:31:54 +0600

    mob change

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+	$('.step-inner').slick({
+		arrows:false,
+		dots:true,
+		infinite:false,
+	});
+})
+
+$(document).ready(function(){
+	$('.diff-inner').slick({
+		arrows:false,
+		dots:true,
+		infinite:false,
+	});
+})
+
+$(document).ready(function(){
+	$('.eq-inner').slick({
+		arrows:false,
+		dots:true,
+		infinite:false,
+	});
+})
+
+$(document).ready(function(){
+	$('.rew-inner').slick({
+		arrows:false,
+		dots:true,
+		infinite:false,
+	});
+})
